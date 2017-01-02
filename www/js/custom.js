71eb59960337fbace7b2c9d0a94770e40984b98b
thanasis <dimitriades.at@gegoweb.com>
2017-01-02 16:33:13 +0200

    Testign with some basic features

--- www/js/custom.js
+++ www/js/custom.js
@@ -9,14 +9,21 @@ var Geolocate = {
 
     findLocation: function(){
 
-        // navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
-        //
-        // function geolocationSuccess(position){
-        //     console.log(position);
-        // }
-        // function geolocationError(err){
-        //     console.log(err);
-        // }
+        navigator.geolocation.watchPosition(geolocationSuccess, geolocationError,
+        []);
+        
+        function geolocationSuccess(position){
+            console.log(position);
+
+            var crd = position.coords;
+
+            document.getElementById('latitude').innerHTML = crd.latitude;
+            document.getElementById('longitude').innerHTML = crd.longitude;
+
+        }
+        function geolocationError(err){
+            console.log(err);
+        }
     }
 }
 
@@ -24,10 +31,14 @@ var Geolocate = {
 
 
 var Accelerate = {
+    
 
     initialize: function(){
+        this.max_g = 0;
         this.findAcceleration();
     },
+
+
 
     findAcceleration: function(){
 
@@ -44,17 +55,29 @@ var Accelerate = {
 
     onSuccess: function(acceleration){
 
+/*
         console.log('Acceleration X: ' + acceleration.x + '\n' +
             'Acceleration Y: ' + acceleration.y + '\n' +
             'Acceleration Z: ' + acceleration.z + '\n' +
             'Timestamp: '      + acceleration.timestamp + '\n');
+*/
 
-        var acc_x = document.getElementById('acc_x');
-        acc_x.innerHTML = acceleration.x;
-        var acc_y = document.getElementById('acc_y');
-        acc_y.innerHTML = acceleration.y;
-        var acc_z = document.getElementById('acc_z');
-        acc_z.innerHTML = acceleration.z;
+        var g = Math.sqrt(acceleration.x * acceleration.x + acceleration.y * acceleration.y + acceleration.z * acceleration.z);
+
+        document.getElementById('current_acceleration').innerHTML = Math.round(g * 100) /100;
+
+        if (g > Accelerate.max_g){
+            Accelerate.max_g = g;
+            document.getElementById('max_acceleration').innerHTML = Math.round(g * 100) /100;;
+        }
+
+
+        // var acc_x = document.getElementById('acc_x');
+        // acc_x.innerHTML = Math.round(acceleration.x * 100)/100;
+        // var acc_y = document.getElementById('acc_y');
+        // acc_y.innerHTML = Math.round(acceleration.y * 100)/100;
+        // var acc_z = document.getElementById('acc_z');
+        // acc_z.innerHTML = Math.round(acceleration.z * 100)/100;
 
     },
 
