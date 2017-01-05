480f76ab82669fb82b0a3473fac2efc6825f33f8
thanasis <dimitriades.at@gegoweb.com>
2017-01-05 12:20:27 +0200

    Update

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -37,6 +37,58 @@ var app = {
         app.receivedEvent('deviceready');
         Geolocate.initialize();
         Accelerate.initialize();
+
+        var push = PushNotification.init({
+            "android": {
+                "senderID": "17661035641"
+            },
+            "browser": {},
+            "ios": {
+                "sound": true,
+                "vibration": true,
+                "badge": true
+            },
+            "windows": {}
+        });
+        PushNotification.hasPermission(function(data) {
+            console.log("STRINGIFY", JSON.stringify(data));
+            if (data.isEnabled) {
+                console.log('isEnabled');
+            }
+        });
+
+        push.on('registration', function(data) {
+            console.log('registration event: ' + data.registrationId);
+
+            var oldRegId = localStorage.getItem('registrationId');
+            if (oldRegId !== data.registrationId) {
+                // Save new registration ID
+                localStorage.setItem('registrationId', data.registrationId);
+                // Post registrationId to your app server as the value has changed
+            }
+
+            var parentElement = document.getElementById('registration');
+            var listeningElement = parentElement.querySelector('.waiting');
+            var receivedElement = parentElement.querySelector('.received');
+
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+        });
+
+        push.on('error', function(e) {
+            console.log("push error = " + e.message);
+        });
+
+        push.on('notification', function(data) {
+            console.log('notification event');
+            navigator.notification.alert(
+                data.message,         // message
+                null,                 // callback
+                data.title,           // title
+                'Ok'                  // buttonName
+            );
+        });
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -48,5 +100,26 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+
+
+    sendAjax: function(){
+
+        var http = new XMLHttpRequest();
+        var url = "https://jsonplaceholder.typicode.com/posts/1";
+        var params = "lorem=ipsum&name=binny";
+        http.open("GET", url, true);
+
+        //Send the proper header information along with the request
+        //http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        http.onreadystatechange = function() {
+            //Call a function when the state changes.
+            console.log('ONREADYSTATE');
+            if(http.readyState == 4 && http.status == 200) {
+                document.getElementById('ajax_result').innerHTML = http.responseText;
+            }
+        }
+        http.send(params);
     }
 };
